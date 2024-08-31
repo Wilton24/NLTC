1db89d0b14d298ec7e36ce15b93e899f70ca1fd5
Wilton24 <cbasher24@gmail.com>
2024-08-31 10:17:26 +0800

    registration initial success Modal

--- modals/SuccessModal.vue
+++ modals/SuccessModal.vue
@@ -1,35 +1,39 @@
 <template>
-  <!-- <div v-if="isOpen" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"> -->
-    <div class="bg-white p-6 rounded-lg shadow-lg max-w-md w-full py-10">
-      <div>
-        <!-- <Icon name="mdi:check" class="w-40 h-40 text-green-600 mx-auto scale-x-4 scale-y-4" /> -->
+    <div class="bg-slate-100 py-4 rounded-lg shadow-lg max-w-md w-full">
+
+      <div class="flex justify-center">        
          <CheckIcon width="150px" height="150px"/>
       </div>
-      <h2 class="text-2xl font-bold mb-4 text-center text-green-600">Success!</h2>
-      <p class="text-gray-700 text-center mb-6">{{ message }}</p>
-      <div class="text-center">
-        <button @click="closeModal" class="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700">
+
+      <h2 class="text-2xl font-bold py-3 mb-4 text-center text-green-600">Success!</h2>
+      <p class="text-gray-700 text-center py-2 mb-6">{{ message }}</p>
+
+      <div class="flex justify-center items-center">
+        <button @click="closeModal" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-10 rounded">
           OK
         </button>
       </div>
     </div>
-  <!-- </div> -->
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 import CheckIcon from '~/assets/icons/CheckIcon.vue';
+import { useRegistrationStore } from '~/store/registrationStore';
+
 
 interface ModalProps {
   message: string;
-}
+};
+
+const registerStore = useRegistrationStore();
 
 const props = defineProps<ModalProps>();
 
 const isOpen = ref(true);
 
 const closeModal = () => {
-  isOpen.value = false;
+  registerStore.isRegistrationSuccess = false;
 };
 </script>
 
--- store/registrationStore.ts
+++ store/registrationStore.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import apiClient from "../utils/apiClient";
 
 export interface IRegistrationData {
